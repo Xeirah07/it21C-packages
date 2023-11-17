aac2f03a90a888e51016cc3f2ff9560b4d18a720
Xeirah07 <suyatsarah519@gmail.com>
2023-11-17 10:55:35 +0800

    created another OOP.basic package

---
+++ src/OOP/basic/Main.java
@@ -0,0 +1,10 @@
+package OOP.basic;
+
+
+public class Main {
+   public static void main(String [] args){
+       Myself s= new Myself("Sarah", "tired", "Ang mo give up, pildi");
+       
+       System.out.println(s.getName() + " is " + s.getFelt() + " pero " + s.getMotto());
+   }
+}
